adb2ea27731b64ae41fffd37d300cc7508853048
ivancasaof <ivancasaof@gmail.com>
2024-03-17 17:21:06 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    HttpClientModule,
+    ProtheusLibCoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ProAppConfigService } from '@totvs/protheus-lib-core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  constructor(private proAppConfigService: ProAppConfigService){}
+
+  click(){
+    if (this.proAppConfigService.insideProtheus()) {
+      this.proAppConfigService.callAppClose();
+    } else {
+      alert('O App não está sendo executado dentro do Protheus.');
+    }
+  }
+}
